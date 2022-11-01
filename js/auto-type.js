ee24bfb8f84d7cc976b161ded55858b67a89026c
kacperwadecki <44477935+kacperwadecki@users.noreply.github.com>
2022-11-02 00:06:09 +0100

    add homepage

---
+++ js/auto-type.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Programowanie Stron Internetowych", "Matematyka - Moja Pasja"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    showCursor: false
+})

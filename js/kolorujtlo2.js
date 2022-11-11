463a5c79d427ac2252321228a7b66a535fd84ecf
kacperwadecki <44477935+kacperwadecki@users.noreply.github.com>
2022-11-11 19:43:04 +0100

    update applications

--- js/kolorujtlo2.js
+++ js/kolorujtlo2.js
@@ -9,4 +9,7 @@ const switchColor = (color) => {
     document.body.style.background = "none";
     document.body.style.backgroundColor = color;
     colorPicker.value = color;
+    if(color=="123"){
+        document.body.style.background = "linear-gradient(var(--gradient-color), var(--gradient-color)), url('../img/index-01.jpg')"; 
+    }
 }
